b614acc5b056a093e45580e305abde7ca21174ca
DanielJBailey <daniel.justin.bailey@gmail.com>
2019-08-20 15:33:27 +0200

    add bcrypt and nodemon and fix up user model to encrypt password

--- webserver/data-models/User.js
+++ webserver/data-models/User.js
@@ -1,17 +1,32 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
-  id: { type: Schema.Types.ObjectId },
-  firstName: { type: String, default: null },
-  lastName: { type: String, default: null },
-  dob: { type: String, default: null }
-})
+  _id: { type: Schema.Types.ObjectId },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  budgets: {
+    type: [Schema.Types.ObjectId],
+    ref: 'Budget'
+  }
+});
 
-const model = mongoose.model('user', UserSchema)
+UserSchema.pre('save', function(next) {
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+      this.password = hash;
+      next();
+    });
+  });
+});
+
+const model = mongoose.model('User', UserSchema);
 
 module.exports = {
   UserModel: model,
   UserSchema,
   default: UserSchema
-}
+};
